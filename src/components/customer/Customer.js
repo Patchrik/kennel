1f82613affe48b4486218803bc2feaef56bc65bc
Parker Kelley <Pklly92@gmail.com>
2020-10-05 13:38:30 -0500

    this should have all the parts needed moving forward.

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.css";
+
+export const CustomerCard = () => (
+	<section className="customer">
+		<h3 className="customer__name">Privacy Pete</h3>
+		<div className="customer__address">100 Nonyabizess</div>
+	</section>
+);
---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Location.css";
+
+export const LocationCard = () => (
+	<section className="Location">
+		<h3 className="Location__name">Nashville Kennel West</h3>
+		<div className="Location__address">151 Rum Drive</div>
+	</section>
+);
