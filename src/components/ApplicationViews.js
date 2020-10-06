515b6c427297950ea4e443199599ad70d96c9a9f
Parker Kelley <Pklly92@gmail.com>
2020-10-06 18:08:19 -0500

    Added some data providers.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,42 +1,47 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { Home } from "./Home";
-import { AnimalCard } from "./animal/AnimalCard";
-import { EmployeeCard } from "./employee/Employee";
-import { CustomerCard } from "./customer/Customer";
-import { LocationCard } from "./location/Location";
-import { AnimalList } from "./animal/AnimalList";
-import { AnimalProvider } from "./animal/AnimalProvider";
-import { CustomerList } from "./customer/CustomerList";
-import { CustomerProvider } from "./customer/CustomerProvider";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Home } from './Home';
+import { AnimalCard } from './animal/AnimalCard';
+import { EmployeeCard } from './employee/Employee';
+import { CustomerCard } from './customer/Customer';
+import { LocationCard } from './location/Location';
+import { AnimalList } from './animal/AnimalList';
+import { AnimalProvider } from './animal/AnimalProvider';
+import { CustomerList } from './customer/CustomerList';
+import { CustomerProvider } from './customer/CustomerProvider';
+import { EmployeeList } from './employee/EmployeeList';
+import { EmployeeProvider } from './employee/EmployeeProvider';
 
 export const ApplicationViews = (props) => {
-	return (
-		<>
-			{/* Render the location list when http://localhost:3000/ */}
-			<Route exact path="/">
-				<Home />
-			</Route>
+  return (
+    <>
+      {/* Render the location list when http://localhost:3000/ */}
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-			{/* Render the animal list when http://localhost:3000/animals */}
-			<AnimalProvider>
-				<Route exact path="/animals">
-					<AnimalList />
-				</Route>
-			</AnimalProvider>
+      {/* Render the animal list when http://localhost:3000/animals */}
+      <AnimalProvider>
+        <Route exact path="/animals">
+          <AnimalList />
+        </Route>
+      </AnimalProvider>
 
-			<Route path="/employees">
-				<EmployeeCard />
-			</Route>
+      <EmployeeProvider>
+        <Route exact path="/employess">
+          <EmployeeList />
+        </Route>
+      </EmployeeProvider>
 
-			<CustomerProvider>
-				<Route path="/customers">
-					<CustomerList />
-				</Route>
-			</CustomerProvider>
-			<Route path="/locations">
-				<LocationCard />
-			</Route>
-		</>
-	);
+      <CustomerProvider>
+        <Route exact path="/customers">
+          <CustomerList />
+        </Route>
+      </CustomerProvider>
+
+      <Route path="/locations">
+        <LocationCard />
+      </Route>
+    </>
+  );
 };
